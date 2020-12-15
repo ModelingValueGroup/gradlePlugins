83c3822fe0e70332598e566c234ec684dfb80b11
Tom Brus <tombrus@gmail.com>
2020-12-15 23:49:37 +0100

    add headers

--- info.kt
+++
@@ -1,4 +0,0 @@
-val correctorName = "mvgCorrector" //secured in test idCheck
-val correctorId = "org.modelingvalue.gradle.corrector" //secured in test idCheck
-val correctorVersion = "0.1"
-val correctorClass = "org.modelingvalue.gradle.corrector.MvgCorrectorPlugin" //secured in test idCheck
