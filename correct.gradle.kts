3b52d33d9654dbe854ca7895427709625d2d974d
Tom Brus <tombrus@gmail.com>
2020-12-17 23:58:18 +0100

    try

--- correct.gradle.kts
+++ correct.gradle.kts
@@ -14,5 +14,5 @@
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 plugins {
-    id("org.modelingvalue.gradle.corrector")/* version "0.3.0"*/
+    id("org.modelingvalue.gradle.corrector") version "0.3.2"
 }
