68363d0f8c810fdc9156ed8830388913bfbf81e2
Tom Brus <tombrus@gmail.com>
2020-12-17 01:06:24 +0100

    try

--- try.gradle.kts
+++ try.gradle.kts
@@ -13,53 +13,10 @@
 //     Arjan Kok, Carel Bast                                                                                           ~
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-val VERSION: String by project
-val GROUP: String by project
-val corrector_name: String by project
-val corrector_id: String by project
-val corrector_version: String? by project
-val corrector_class: String by project
-val corrector_displayname: String by project
-
-group = GROUP
-version = VERSION
-
 plugins {
-    `java-gradle-plugin`
-    id("com.gradle.plugin-publish") version "0.12.0"
-}
-repositories {
-    jcenter()
-}
-tasks.test {
-    useJUnitPlatform()
-}
-dependencies {
-    implementation("org.eclipse.jgit:org.eclipse.jgit:5.9.0.202009080501-r")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    id("org.modelingvalue.gradle.corrector") version "0.3.0"
 }
 
-gradlePlugin {
-    plugins.create(corrector_name) {
-        id = corrector_id
-        implementationClass = corrector_class
-        displayName = corrector_displayname
-        version = corrector_version ?: VERSION
-    }
-}
-
-pluginBundle {
-    website = "http://www.modelingvalue.org/"
-    vcsUrl = "https://github.com/ModelingValueGroup/gradlePlugins"
-    description = "MVG gradle plugins"
-    (plugins) {
-        corrector_name {
-            tags = listOf("mvg")
-        }
-    }
-}
-
-tasks.withType<Javadoc> {
-    options.encoding = "UTF-8"
+mvgCorrector {
+    gitPush = true
 }
