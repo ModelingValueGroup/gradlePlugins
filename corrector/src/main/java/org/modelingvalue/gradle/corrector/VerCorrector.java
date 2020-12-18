776d9d784a2245275f4c721770a21a9c5da2b2f7
automation <automation@modelingvalue.org>
2020-12-18 20:48:45 +0100

    ~changed by corrector

---
+++ corrector/src/main/java/org/modelingvalue/gradle/corrector/VerCorrector.java
@@ -0,0 +1,9 @@
+package org.modelingvalue.gradle.corrector;
+
+import java.nio.file.Path;
+
+public class VersionCorrector {
+    public VersionCorrector(Path file, String var) {
+        
+    }
+}
