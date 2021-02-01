e07f01421d337822b18861cac2e80a327e69d478
Tom Brus <tombrus@gmail.com>
2021-02-01 11:33:49 +0100

    allow mps version to be set in properties file

--- mvgplugin/src/test/resources/org/modelingvalue/gradle/build.gradle.kts
+++ mvgplugin/src/test/resources/org/modelingvalue/gradle/build.gradle.kts
@@ -6,9 +6,6 @@ plugins {
 `~myMvgCorrectorExtension~` {
     addTextFileExtension("pruuperties")
 }
-`~myMvgMpsExtension~` {
-    version = "2020.3"
-}
 dependencies {
     implementation("demo-lib:lib:3.0.52-BRANCHED")
 
