c6c56df4ef5732a5112c8c7cedcd5124da7a1963
Tom Brus <tombrus@gmail.com>
2021-10-08 16:12:16 +0200

    bump junit version

--- mvgplugin/build.gradle.kts
+++ mvgplugin/build.gradle.kts
@@ -42,8 +42,8 @@ dependencies {
     implementation("com.gradle:gradle-enterprise-gradle-plugin:3.6.1")
     implementation("org.apache.httpcomponents:httpmime:4.5.13")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
 
 gradlePlugin {
