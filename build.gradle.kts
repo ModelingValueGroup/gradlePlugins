e81fc87c9fa4eb7ed38ea519240dad62190b8581
Tom Brus <tombrus@gmail.com>
2021-11-02 11:20:01 +0100

    mvgplugin -> 1.0.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@
 defaultTasks("mvgCorrector", "test", "publishPlugins", "mvgTagger")
 
 plugins {
-    id("org.modelingvalue.gradle.mvgplugin") version ("1.0.7")
+    id("org.modelingvalue.gradle.mvgplugin") version ("1.0.8")
 }
 mvgcorrector {
     addHeaderFileExclude("mvgplugin/src/test/resources/.*")
