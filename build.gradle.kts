2c301f07dbbdccede98e380520f11f71873558fa
Tom Brus <tombrus@gmail.com>
2026-02-24 13:28:31 +0100

    chore: update GitHub Actions for gh-pages publishing and bump MVG plugin to 2.0.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@
 defaultTasks("test", "publishPlugins")
 
 plugins {
-    id("org.modelingvalue.gradle.mvgplugin") version ("2.0.2")
+    id("org.modelingvalue.gradle.mvgplugin") version ("2.0.4")
 }
 mvgcorrector {
     addHeaderFileExclude("mvgplugin/src/test/resources/.*")
